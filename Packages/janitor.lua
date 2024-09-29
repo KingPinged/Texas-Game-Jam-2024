d61c98c3fc5908e47b78b589ebc2df5eeecbc1e6
KingPing <kingsinland@gmail.com>
2024-09-29 05:46:45 -0500

    Renamed files to fit better default project json and then updated sourcemap and wally types

--- Packages/janitor.lua
+++ Packages/janitor.lua
@@ -1,1 +1,3 @@
-return require(script.Parent._Index["howmanysmall_janitor@1.17.0"]["janitor"])
+local REQUIRED_MODULE = require(script.Parent._Index["howmanysmall_janitor@1.17.0"]["janitor"])
+export type Janitor = REQUIRED_MODULE.Janitor 
+return REQUIRED_MODULE
